e24fa891f23b8e7c45f666d001e2d6069416c9a5
takemehigh <370503306@qq.com>
2015-12-01 18:35:30 +0800

    tankwar

--- TankWar0.1/src/TankClient.java
+++ TankWar0.1/src/TankClient.java
@@ -1,12 +1,25 @@
 import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 public class TankClient extends Frame{
 	/**
 	 * 
 	 */
 	private void launchFrame(){
 		this.setSize(800, 600);
+		this.setLocation(300, 400);
 		this.setTitle("Ì¹¿Ë´óÕ½0.1");
+		this.setResizable(false);
 		this.setVisible(true);
+		this.addWindowListener(new WindowAdapter() {
+
+			@Override
+			public void windowClosing(WindowEvent e) {
+				// TODO Auto-generated method stub
+				System.exit(0);
+			}
+			
+		});
 	}
 	public static void main(String[] args) {
 		new TankClient().launchFrame();

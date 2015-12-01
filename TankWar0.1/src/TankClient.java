5b8eed2169822976947acd2aabc23629dcf47bbf
takemehigh <370503306@qq.com>
2015-12-01 18:11:51 +0800

    tankwar0.1

---
+++ TankWar0.1/src/TankClient.java
@@ -0,0 +1,15 @@
+import java.awt.*;
+public class TankClient extends Frame{
+	/**
+	 * 
+	 */
+	private void launchFrame(){
+		this.setSize(800, 600);
+		this.setTitle("Ì¹¿Ë´óÕ½0.1");
+		this.setVisible(true);
+	}
+	public static void main(String[] args) {
+		new TankClient().launchFrame();
+	}
+	
+}
